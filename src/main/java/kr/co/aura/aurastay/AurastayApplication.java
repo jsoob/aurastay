3b80dbbe6e4b05da3853142b168c3041a29fac50
JS <kmhe0128@naver.com>
2025-04-02 12:26:16 +0900

    파일 경로 ubuntu로 변경 (upload.directory) war 파일로 배포 변경 (id 'war') SpringBootServletInitializer (web.xml이 없는 SpringBoot 웹 애플리케이션을 외부 Tomcat에서 동작하도록 하기 위해서는 WebApplicationInitializer 인터페이스를 구현한 SpringBootServletInitializer를 상속을 받는 것이 필요)

--- src/main/java/kr/co/aura/aurastay/AurastayApplication.java
+++ src/main/java/kr/co/aura/aurastay/AurastayApplication.java
@@ -3,12 +3,19 @@ package kr.co.aura.aurastay;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@MapperScan("kr.co.aura.aurastay.repository")
 @SpringBootApplication
 @EnableScheduling // 스케줄러 활성화
-public class AurastayApplication {
+public class AurastayApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AurastayApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AurastayApplication.class, args);
