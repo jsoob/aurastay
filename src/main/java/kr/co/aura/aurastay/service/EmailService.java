f6815597676466c2fc40c9d6cb98e1e44c4c9f3d
YYR <yeeefla@naver.com>
2025-04-03 15:27:22 +0900

    email 수정

--- src/main/java/kr/co/aura/aurastay/service/EmailService.java
+++ src/main/java/kr/co/aura/aurastay/service/EmailService.java
@@ -37,12 +37,12 @@ public class EmailService {
                 String htmlContent = null;
                 if(type.equals("email")){
                 htmlContent = new String(Files.readAllBytes(
-                        Paths.get("/upload/email-template.jsp")),
+                        Paths.get("/WEB-INF/views/email/email-template.jsp")),
                         StandardCharsets.UTF_8
                 );
                 } else if(type.equals("password")){
                     htmlContent = new String(Files.readAllBytes(
-                            Paths.get("/upload/email-template2.jsp")),
+                            Paths.get("/WEB-INF/views/email/email-template2.jsp")),
                             StandardCharsets.UTF_8);
                 }
 
